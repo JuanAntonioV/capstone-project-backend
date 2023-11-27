9eb794bfaaea57c6378a027918a8cc48f1514830
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-11-27 11:28:20 +0700

    Add app details to getAppDetails function

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,27 @@
 const { okResponse } = require('../utils/response');
 
 const getAppDetails = async (req, res) => {
-    okResponse(res, {
-        message: 'Hello World',
-    });
+    const name = 'Laundry API';
+    const version = '1.0.0';
+    const author = 'Juan Antonio and Team';
+    const description =
+        'This is a REST API application made with Node.js, Express, and Mysql.';
+
+    const packagesVersion = {
+        express: '^4.18.2',
+        node: '^20.9.0',
+        npm: '^10.2.1',
+    };
+
+    const appDetails = {
+        appName: name,
+        appVersion: version,
+        appAuthor: author,
+        appDescription: description,
+        packagesVersion: packagesVersion,
+    };
+
+    okResponse(res, appDetails);
 };
 
 module.exports = {
