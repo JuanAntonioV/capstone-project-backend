3576f28549111547c5d9845c104160ef45c276e8
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-11-28 11:28:23 +0700

    Refactor sales and sales detail tables

--- migrations/20231125023925-create-sales.js
+++ migrations/20231125023925-create-sales.js
@@ -1,46 +1,45 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Sales', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.BIGINT
-      },
-      user_id: {
-        type: Sequelize.BIGINT,
-        references:{
-          model:'Users',
-          key:'id'
-        }
-
-      },
-      category_id: {
-        type: Sequelize.BIGINT,
-        references:{
-          model:'Categories',
-          key:'id'
-        }
-      },
-      total_payment: {
-        type: Sequelize.BIGINT
-      },
-      status: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Sales');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Sales', {
+            id: {
+                allowNull: false,
+                autoIncrement: false,
+                primaryKey: true,
+                type: Sequelize.STRING(50),
+            },
+            user_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'Users',
+                    key: 'id',
+                },
+            },
+            category_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'Categories',
+                    key: 'id',
+                },
+            },
+            total_payment: {
+                type: Sequelize.BIGINT,
+            },
+            status: {
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Sales');
+    },
 };
--- migrations/20231125023927-create-sales-detail.js
+++ migrations/20231125023927-create-sales-detail.js
@@ -1,45 +1,42 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Sales_Details', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.BIGINT
-      },
-      sales_id: {
-        type: Sequelize.BIGINT,
-        references:{
-          model:'Sales',
-          key:'id'
-        }
-      },
-      product_id: {
-        type: Sequelize.BIGINT,
-        references:{
-          model:'Products',
-          key:'id'
-        }
-      },
-      quantity: {
-        type: Sequelize.INTEGER
-      },
-      amount: {
-        type: Sequelize.BIGINT
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Sales_Details');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Sales_Details', {
+            sales_id: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+                autoIncrement: false,
+                primaryKey: true,
+                references: {
+                    model: 'Sales',
+                    key: 'id',
+                },
+            },
+            product_id: {
+                type: Sequelize.BIGINT,
+                references: {
+                    model: 'Products',
+                    key: 'id',
+                },
+            },
+            quantity: {
+                type: Sequelize.INTEGER,
+            },
+            amount: {
+                type: Sequelize.BIGINT,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Sales_Details');
+    },
 };
