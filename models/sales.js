3e9cc0c63f989ee721f30691c222c2b48dc69f38
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-11-28 11:29:08 +0700

    Refactor model associations in sales.js and sales_detail.js

--- models/sales.js
+++ models/sales.js
@@ -1,30 +1,31 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Sales extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Sales.belongsTo(models.User, { foreignKey: 'id_User' });
-      Sales.belongsTo(models.Category, { foreignKey: 'id_Category' });
+    class Sales extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Sales.belongsTo(models.User, { foreignKey: 'id_User' });
+            Sales.belongsTo(models.Category, { foreignKey: 'id_Category' });
 
-      Sales.hasMany(models.Sales_Detail, { foreignKey: 'id_Sales' });
+            Sales.hasMany(models.Sales_Detail, { foreignKey: 'id_Sales' });
+        }
     }
-  }
-  Sales.init({
-    user_id: DataTypes.BIGINT,
-    category_id: DataTypes.BIGINT,
-    total_payment: DataTypes.BIGINT,
-    status: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Sales',
-  });
-  return Sales;
+    Sales.init(
+        {
+            user_id: DataTypes.BIGINT,
+            category_id: DataTypes.BIGINT,
+            total_payment: DataTypes.BIGINT,
+            status: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'Sales',
+        }
+    );
+    return Sales;
 };
--- models/sales_detail.js
+++ models/sales_detail.js
@@ -1,28 +1,31 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Sales_Detail extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      Sales_Detail.belongsTo(models.Sales, { foreignKey: 'id_Sales' });
-      Sales_Detail.belongsTo(models.Product, { foreignKey: 'id_Product' });
+    class Sales_Detail extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            Sales_Detail.belongsTo(models.Sales, { foreignKey: 'id_Sales' });
+            Sales_Detail.belongsTo(models.Product, {
+                foreignKey: 'id_Product',
+            });
+        }
     }
-  }
-  Sales_Detail.init({
-    sales_id: DataTypes.BIGINT,
-    product_id: DataTypes.BIGINT,
-    quantity: DataTypes.INTEGER,
-    amount: DataTypes.BIGINT
-  }, {
-    sequelize,
-    modelName: 'Sales_Detail',
-  });
-  return Sales_Detail;
+    Sales_Detail.init(
+        {
+            sales_id: DataTypes.STRING(50),
+            product_id: DataTypes.BIGINT,
+            quantity: DataTypes.INTEGER,
+            amount: DataTypes.BIGINT,
+        },
+        {
+            sequelize,
+            modelName: 'Sales_Detail',
+        }
+    );
+    return Sales_Detail;
 };
