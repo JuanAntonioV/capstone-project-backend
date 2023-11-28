01f2762e910557968b4c562bcd51de09f1f57fb3
AlfianFR1 <rohmanfatur.alfian@gmail.com>
2023-11-28 22:42:07 +0700

    add defaultvalue status

--- migrations/20231125023921-create-category.js
+++ migrations/20231125023921-create-category.js
@@ -13,7 +13,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       status: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue :true,
       },
       createdAt: {
         allowNull: false,
