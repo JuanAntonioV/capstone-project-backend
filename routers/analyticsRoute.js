a331d926126ca8fedcb53d8c595fd9045070e4d8
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-12-28 10:50:16 +0700

    Remove authentication middleware from export-sales route

--- routers/analyticsRoute.js
+++ routers/analyticsRoute.js
@@ -6,5 +6,5 @@ const { checkUserToken } = require('../middlewares/authMiddleware');
 
 module.exports = (router) => {
     router.get('/stats', checkUserToken, getDashboardStats);
-    router.get('/export-sales', checkUserToken, getExportSales);
+    router.get('/export-sales', getExportSales);
 };
